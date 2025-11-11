57373f872dd071b9fd90e7ebe4f94660e63c851a
morasaikrishna2006-droid <mora.saikrishna2006@gmail.com>
2025-11-11 15:35:35 +0530

    feat: Enable PWA functionality

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,6 @@ import { VitePWA } from 'vite-plugin-pwa';
 export default defineConfig({
   plugins: [
     react(),
-    /*
     VitePWA({
       registerType: 'autoUpdate',
       includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'mask-icon.svg'],
@@ -15,20 +14,21 @@ export default defineConfig({
         short_name: 'LushLook',
         description: 'Connecting you with the best beauty services at home.',
         theme_color: '#ffffff',
+        background_color: '#ffffff',
+        display: 'standalone',
         icons: [
           {
             src: '/icons/icon-192x192.png',
             sizes: '192x192',
-            type: 'image/png'
+            type: 'image/png',
           },
           {
             src: '/icons/icon-512x512.png',
             sizes: '512x512',
-            type: 'image/png'
-          }
+            type: 'image/png',
+          },
         ]
       }
     })
-    */
   ],
 })
